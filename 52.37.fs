f58210b4912c37aeeeda61cdd00792631bdee1bf
Joe Chavez <joe@izen.me>
2021-09-10 12:44:34 -0700

    52.37 WIP

--- 52.37.fs
+++ 52.37.fs
@@ -14,21 +14,43 @@
 */
 
 
+float permuteRate = 289. * 1.; // 289.
+float gFactor = 7.; // 7.
+float fadeFactor = 2.2; // 2.2
+float amp = 2.2; // 2.2
+float smoothing = 1.; // 34.0
+float xPos = .5; // 0.5
+float yPos = .2; // 0.2
+float yPosNoise = .5; // 0.5
+float uvPos = 0.5; // 0.5
+float lumi = 0.3; // 0.3
+float numPlots = 30.; // 30.
+float zPos = 0.02; // 0.02
+float cut = 0.0073; // 0.0073
 
-//	Classic Perlin 3D Noise 
-//	by Stefan Gustavson
-//
-vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
-vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
-vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+vec4 permute(vec4 x) {
+    return mod(((x * smoothing) + 1.) * x, permuteRate);
+}
+
+vec4 taylorInvSqrt(vec4 r) {
+    // return 1.79284291400159 - 0.85373472095314 * r;
+    return (1.79284291400159 - 0.85373472095314 * r) * amp;
+}
+
+vec3 fade(vec3 t) {
+    return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
 
 float cnoise(vec3 P){
   vec3 Pi0 = floor(P); // Integer part for indexing
   vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
-  Pi0 = mod(Pi0, 289.0);
-  Pi1 = mod(Pi1, 289.0);
+
+  Pi0 = mod(Pi0, permuteRate);
+  Pi1 = mod(Pi1, permuteRate);
+
   vec3 Pf0 = fract(P); // Fractional part for interpolation
   vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+
   vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
   vec4 iy = vec4(Pi0.yy, Pi1.yy);
   vec4 iz0 = Pi0.zzzz;
@@ -38,21 +60,21 @@ float cnoise(vec3 P){
   vec4 ixy0 = permute(ixy + iz0);
   vec4 ixy1 = permute(ixy + iz1);
 
-  vec4 gx0 = ixy0 / 7.0;
-  vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+  vec4 gx0 = ixy0 / gFactor;
+  vec4 gy0 = fract(floor(gx0) / gFactor) - yPosNoise;
   gx0 = fract(gx0);
-  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 gz0 = vec4(yPosNoise) - abs(gx0) - abs(gy0);
   vec4 sz0 = step(gz0, vec4(0.0));
-  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
-  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+  gx0 -= sz0 * (step(0.0, gx0) - yPosNoise);
+  gy0 -= sz0 * (step(0.0, gy0) - yPosNoise);
 
-  vec4 gx1 = ixy1 / 7.0;
-  vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+  vec4 gx1 = ixy1 / gFactor;
+  vec4 gy1 = fract(floor(gx1) / gFactor) - yPosNoise;
   gx1 = fract(gx1);
-  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 gz1 = vec4(yPosNoise) - abs(gx1) - abs(gy1);
   vec4 sz1 = step(gz1, vec4(0.0));
-  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
-  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+  gx1 -= sz1 * (step(0.0, gx1) - yPosNoise);
+  gy1 -= sz1 * (step(0.0, gy1) - yPosNoise);
 
   vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
   vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
@@ -87,35 +109,32 @@ float cnoise(vec3 P){
   vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
   vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
   float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
-  return 2.2 * n_xyz;
+  return fadeFactor * n_xyz;
 }
 
 float plot(vec2 st, float pct){
-  float cut = 0.0073;
-  return  smoothstep( pct-cut, pct, st.y) -
-          smoothstep( pct, pct+cut, st.y);
+  return  smoothstep( pct - cut, pct, st.y) -
+          smoothstep( pct, pct + cut, st.y);
 }
 
 void main() {
 
-
-
-    vec2 uv =(gl_FragCoord.xy - 0.5 * RENDERSIZE.xy)/RENDERSIZE.y; // -.5 <> .5
+    vec2 uv =(gl_FragCoord.xy - 0.5 * RENDERSIZE.xy)/RENDERSIZE.y; // -uvPos <> uvPos
     vec3 col = vec3(0.);
-    for (float i = 0.; i < 30.; i += 1.) {
-        float inc = i / 30.;
-        float l = plot(
-            uv * .5, 0.13 * cnoise(
-            vec3(
-                smoothstep(0.5, 0., .4 * length(uv)) * 4. + TIME, 
-                .2 * TIME + .2 * uv.x,
-                -i * 0.02
-            )
-           ));
-        vec3 c = mix(vec3(1., uv.x + .5, 1. - uv.x), vec3(uv.x + .5, .2, .5 - uv.x), inc);
-        col += vec3(l * c * .3);
+    for (float i = 0.; i < numPlots; i += 1.) {
+        float inc = i / numPlots;
+
+        float px = smoothstep(xPos, 0., .4 * length(uv)) * 4. + TIME;
+        float py = yPos * TIME + yPos * uv.x;
+        vec3 P = vec3(px, py, -i * zPos);
+
+        float l = plot(uv * uvPos, 0.13 * cnoise(P));
+
+        vec3 c = mix(vec3(1., uv.x + uvPos, 1. - uv.x), vec3(uv.x + uvPos, .2, uvPos - uv.x), inc);
+
+        col += vec3(l * c * lumi);
     }
     
     // Output to screen
-    gl_FragColor = vec4(col,1.);
+    gl_FragColor = vec4(col, 1.);
 }
