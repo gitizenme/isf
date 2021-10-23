0b13adaa72a2ce77e4f582957f28add1446c5d1c
Joe Chavez <joe@izen.me>
2021-10-23 08:50:38 -0700

    52.43 ready for visuals

--- 52.43.fs
+++ 52.43.fs
@@ -71,6 +71,10 @@
 }
 */
 
+float square(float s) { return s * s; }
+vec3 square(vec3 s) { return s * s; }
+
+
 #define saturate(oo) clamp(oo, 0.0, 1.0)
 #define MarchSteps 4
 #define Radius 1.5
@@ -172,26 +176,43 @@ vec3 hueGradient(float t, vec3 bc) {
 	return (clamp(p - 1.0, 0.0, 1.0));
 }
 
-vec3 techGradient(float t, vec3 bc) {
-	return pow(vec3(t + 0.01), bc * vec3(120.0, 10.0, 180.0));
-}
-
 vec3 fireGradient(float t) {
 	return max(pow(vec3(min(t * 1.02, 1.0)), vec3(1.7, 25.0, 100.0)), 
 			   vec3(0.06 * pow(max(1.0 - abs(t - 0.35), 0.0), 5.0)));
 }
 
 vec3 fireGradient(float t, vec3 bc) {
-	return max(pow(vec3(min(t * 1.02, 1.0)), vec3(bc.r * 1.7, bc.g * 25.0, bc.b * 100.0)), 
+	return max(pow(vec3(min(t * 1.02, 1.0)), bc), 
 			   vec3(0.06 * pow(max(1.0 - abs(t - 0.35), 0.0), 5.0)));
 }
 
+vec3 electricGradient(float t) {
+	return clamp( vec3(t * 8.0 - 6.3, square(smoothstep(0.6, 0.9, t)), pow(t, 3.0) * 1.7), 0.0, 1.0);	
+}
 
+
+vec3 neonGradient(float t) {
+	return clamp(vec3(t * 1.3 + 0.1, square(abs(0.43 - t) * 1.7), (1.0 - t) * 1.7), 0.0, 1.0);
+}
+
+
+vec3 heatmapGradient(float t) {
+	return clamp((pow(t, 1.5) * 0.8 + 0.2) * vec3(smoothstep(0.0, 0.35, t) + t * 0.5, smoothstep(0.5, 1.0, t), max(1.0 - t * 1.7, t * 7.0 - 6.0)), 0.0, 1.0);
+}
+
+
+vec3 rainbowGradient(float t) {
+	vec3 c = 1.0 - pow(abs(vec3(t) - baseColor.rgb) * vec3(3.0, 3.0, 5.0), vec3(1.5, 1.3, 1.7));
+	c.r = max((0.15 - square(abs(t - 0.04) * 5.0)), c.r);
+	c.g = (t < 0.5) ? smoothstep(0.04, 0.45, t) : c.g;
+	return clamp(c, 0.0, 1.0);
+}
 
 vec4 Shade(float distance)
 {
 	vec4 g = vec4(hueGradient(distance, baseColor.rgb), 1.);
 	// vec4 g = vec4(fireGradient(distance, baseColor.rgb), 1.);
+	// vec4 g = vec4(rainbowGradient(distance), 1.);
 	return g;
 }
 
